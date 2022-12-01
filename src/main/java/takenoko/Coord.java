1bdacf2996bae9035a9306874daedc80316e0c67
Loïc PALAYER <loic.palayer@etu.univ-cotedazur.fr>
2022-12-01 13:29:23 +0100

    feat: get adjacent Coord table #3

--- src/main/java/takenoko/Coord.java
+++ src/main/java/takenoko/Coord.java
@@ -12,4 +12,15 @@ public record Coord(int x, int y) {
         if (!(o instanceof Coord c)) return false;
         return (x == c.x && y == c.y);
     }
+
+    public Coord[] adjacentCoords() {
+        Coord[] adjacentCoords = new Coord[6];
+        adjacentCoords[0] = new Coord(x, y + 1);
+        adjacentCoords[1] = new Coord(x + 1, y);
+        adjacentCoords[2] = new Coord(x + 1, y - 1);
+        adjacentCoords[3] = new Coord(x, y - 1);
+        adjacentCoords[4] = new Coord(x - 1, y);
+        adjacentCoords[5] = new Coord(x - 1, y + 1);
+        return adjacentCoords;
+    }
 }
--- src/test/java/takenoko/CoordTest.java
+++ src/test/java/takenoko/CoordTest.java
@@ -31,4 +31,16 @@ public class CoordTest {
         assertEquals(new Coord(1, 2), coordx1y2);
         assertNotEquals(new Coord(2, 1), coordx1y2);
     }
+
+    @Test
+    void adjacentCoordsTest() {
+        Coord[] adjacentCoords = coordx1y2.adjacentCoords();
+        assertEquals(6, adjacentCoords.length);
+        assertEquals(new Coord(1, 3), adjacentCoords[0]);
+        assertEquals(new Coord(2, 2), adjacentCoords[1]);
+        assertEquals(new Coord(2, 1), adjacentCoords[2]);
+        assertEquals(new Coord(1, 1), adjacentCoords[3]);
+        assertEquals(new Coord(0, 2), adjacentCoords[4]);
+        assertEquals(new Coord(0, 3), adjacentCoords[5]);
+    }
 }
