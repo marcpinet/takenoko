246744a2db67fd76d8558d504d79caab4fbaec8c
ClementREMY2 <clement.remy@etu.univ-cotedazur.fr>
2022-12-04 16:16:48 +0100

    feat: add method placeTile #5

--- src/main/java/takenoko/TileBoard.java
+++ src/main/java/takenoko/TileBoard.java
@@ -4,10 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TileBoard {
-    private Map<Coord,Tile> tiles;
+    private Map<Coord, Tile> tiles;
 
     public TileBoard() {
         tiles = new HashMap<>();
-        tiles.put(new Coord(0,0),new PondTile());
+        tiles.put(new Coord(0, 0), new PondTile());
+    }
+
+    public void placeTile(Coord c, Tile t) {
+        tiles.put(c, t);
+    }
+
+    public Map<Coord, Tile> getTiles() {
+        return tiles;
     }
 }
--- src/test/java/takenoko/TileBoardTest.java
+++ src/test/java/takenoko/TileBoardTest.java
@@ -1,8 +1,9 @@
 package takenoko;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TileBoardTest {
 
@@ -13,4 +14,13 @@ public class TileBoardTest {
         tileboard = new TileBoard();
     }
 
+    @Test
+    void PlaceTileTest() {
+        Coord c = new Coord(1, 2);
+        Tile t = new BambooTile();
+        tileboard.placeTile(c, t);
+        assertTrue(tileboard.getTiles().containsKey(c));
+        assertTrue(tileboard.getTiles().containsValue(t));
+        assertTrue(tileboard.getTiles().get(c).equals(t));
+    }
 }
