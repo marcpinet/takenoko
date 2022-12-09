54a7f2a1b3b06207774f877be74ab27d1c30b7c0
Edgar Bizel <edgar.bizel@etu.univ-cotedazur.fr>
2022-12-09 15:28:46 +0100

    feat: add equals to several classes

--- src/main/java/takenoko/Action.java
+++ src/main/java/takenoko/Action.java
@@ -1,5 +1,7 @@
 package takenoko;
 
+import java.util.Objects;
+
 public sealed interface Action permits Action.None, Action.PlaceTile {
     Action NONE = new Action.None();
 
@@ -16,6 +18,19 @@ public sealed interface Action permits Action.None, Action.PlaceTile {
         @Override
         public int cost() {
             return 1;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof PlaceTile other) {
+                return coord.equals(other.coord) && tile.equals(other.tile);
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(coord, tile);
         }
     }
 }
