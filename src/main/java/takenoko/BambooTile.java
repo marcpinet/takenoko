6a1114bfc73ac38bf00a5923b78cd26fe7e231a4
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2022-12-15 20:17:51 +0100

    feat: added bamboo support for BambooTile #19

--- src/main/java/takenoko/BambooTile.java
+++ src/main/java/takenoko/BambooTile.java
@@ -1,14 +1,36 @@
 package takenoko;
 
 public class BambooTile implements Tile {
+    private int bambooSize;
+
+    public BambooTile() {
+        bambooSize = 0;
+    }
+
+    public void growBamboo() {
+        if (bambooSize < 4) {
+            bambooSize++;
+        }
+    }
+
+    public int getBambooSize() {
+        return bambooSize;
+    }
+
+    public void shrinkBamboo() {
+        if (bambooSize > 0) {
+            bambooSize--;
+        }
+    }
+
     @Override
     public boolean isCultivable() {
-        return true;
+        return bambooSize < 4;
     }
 
     @Override
     public boolean equals(Object o) {
-        return o instanceof BambooTile;
+        return o instanceof BambooTile bambooTile && bambooTile.bambooSize == bambooSize;
     }
 
     @Override
