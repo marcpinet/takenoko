76736c8ae90ca44ece7ff5c89e98db47d5aaf906
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2022-12-15 20:21:57 +0100

    feat: added tests for bamboo growing/shrinking #19

--- src/test/java/takenoko/BambooTileTest.java
+++ src/test/java/takenoko/BambooTileTest.java
@@ -1,6 +1,6 @@
 package takenoko;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,7 +14,56 @@ public class BambooTileTest {
     }
 
     @Test
-    void isCultivableTest() {
+    void testGrowBamboo() {
+        assertEquals(0, bambooTile.getBambooSize());
+        bambooTile.growBamboo();
+        assertEquals(1, bambooTile.getBambooSize());
+        bambooTile.growBamboo();
+        assertEquals(2, bambooTile.getBambooSize());
+        bambooTile.growBamboo();
+        assertEquals(3, bambooTile.getBambooSize());
+        bambooTile.growBamboo();
+        assertEquals(4, bambooTile.getBambooSize());
+        bambooTile.growBamboo();
+        assertEquals(4, bambooTile.getBambooSize());
+    }
+
+    @Test
+    void testShrinkBamboo() {
+        assertEquals(0, bambooTile.getBambooSize());
+        bambooTile.shrinkBamboo();
+        assertEquals(0, bambooTile.getBambooSize());
+        bambooTile.growBamboo();
+        bambooTile.growBamboo();
+        assertEquals(2, bambooTile.getBambooSize());
+        bambooTile.shrinkBamboo();
+        assertEquals(1, bambooTile.getBambooSize());
+        bambooTile.shrinkBamboo();
+        assertEquals(0, bambooTile.getBambooSize());
+    }
+
+    @Test
+    void testIsCultivable() {
         assertTrue(bambooTile.isCultivable());
+        bambooTile.growBamboo();
+        bambooTile.growBamboo();
+        bambooTile.growBamboo();
+        assertTrue(bambooTile.isCultivable());
+        bambooTile.growBamboo();
+        assertFalse(bambooTile.isCultivable());
+        bambooTile.shrinkBamboo();
+        assertTrue(bambooTile.isCultivable());
+    }
+
+    @Test
+    void testEquals() {
+        BambooTile otherTile = new BambooTile();
+        assertEquals(bambooTile, otherTile);
+        bambooTile.growBamboo();
+        assertNotEquals(bambooTile, otherTile);
+        otherTile.growBamboo();
+        assertEquals(bambooTile, otherTile);
+        otherTile.growBamboo();
+        assertNotEquals(bambooTile, otherTile);
     }
 }
