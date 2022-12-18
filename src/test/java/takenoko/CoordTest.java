5f831d268d9762efa9b4277f3dc78926ff645613
Loïc PALAYER <loic.palayer@etu.univ-cotedazur.fr>
2022-12-18 21:06:23 +0100

    fix: Coord logic and CoordTest #46

--- src/test/java/takenoko/CoordTest.java
+++ src/test/java/takenoko/CoordTest.java
@@ -39,14 +39,14 @@ public class CoordTest {
 
     @Test
     void adjacentCoordsTest() {
-        Coord[] adjacentCoords = coordx1y2.adjacentCoords();
+        Coord[] adjacentCoords = center.adjacentCoords();
         assertEquals(6, adjacentCoords.length);
-        assertEquals(new Coord(1, 3), adjacentCoords[0]);
-        assertEquals(new Coord(2, 2), adjacentCoords[1]);
-        assertEquals(new Coord(2, 1), adjacentCoords[2]);
-        assertEquals(new Coord(1, 1), adjacentCoords[3]);
-        assertEquals(new Coord(0, 2), adjacentCoords[4]);
-        assertEquals(new Coord(0, 3), adjacentCoords[5]);
+        assertEquals(new Coord(0, -1), adjacentCoords[0]);
+        assertEquals(new Coord(1, -1), adjacentCoords[1]);
+        assertEquals(new Coord(1, 0), adjacentCoords[2]);
+        assertEquals(new Coord(0, 1), adjacentCoords[3]);
+        assertEquals(new Coord(-1, 1), adjacentCoords[4]);
+        assertEquals(new Coord(-1, 0), adjacentCoords[5]);
     }
 
     @Test
