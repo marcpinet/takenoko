19cbc70d2e31bbe518a81e9665d4324786f290ea
ClementREMY2 <clement.remy@etu.univ-cotedazur.fr>
2022-12-20 17:43:54 +0100

    feat: implements isAchieved, in BambooSizeObjective #21

---
+++ src/main/java/takenoko/objective/BambooSizeObjective.java
@@ -0,0 +1,42 @@
+package takenoko.objective;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import takenoko.Action;
+import takenoko.BambooSizeException;
+import takenoko.BambooTile;
+import takenoko.Board;
+
+public class BambooSizeObjective implements Objective {
+
+    private final int sizeObjective;
+    private boolean achieved = false;
+
+    public BambooSizeObjective(int b) throws BambooSizeException {
+        if (b < 1 || b > 4) {
+            throw new BambooSizeException("Error : unreachable bamboo size.");
+        }
+        this.sizeObjective = b;
+    }
+
+    @Override
+    public boolean isAchieved(Board board, Action lastAction) {
+        AtomicBoolean res = new AtomicBoolean(false);
+        board.applyOnEachTile(
+                tile -> {
+                    if (!(tile instanceof BambooTile bambooTile)) {
+                        return null;
+                    }
+                    if (bambooTile.getBambooSize() == sizeObjective) {
+                        res.set(true);
+                    }
+                    return null;
+                });
+        achieved = res.get();
+        return achieved;
+    }
+
+    @Override
+    public boolean wasAchievedAfterLastCheck() {
+        return false;
+    }
+}
---
+++ src/test/java/takenoko/objective/BambooSizeObjectiveTest.java
@@ -0,0 +1,111 @@
+package takenoko.objective;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import takenoko.*;
+
+public class BambooSizeObjectiveTest {
+
+    BambooSizeObjective b1, b3, b4;
+    Board board;
+
+    // The board always apply this action first
+    static final Action.PlaceTile INITIAL_ACTION =
+            new Action.PlaceTile(new Coord(0, 0), new PondTile());
+
+    Action.PlaceTile placeBambooTile(Board board, Coord c) {
+        try {
+            board.placeTile(c, new BambooTile());
+        } catch (BoardException e) {
+            fail(e);
+        } catch (IrrigationException e) {
+            fail(e);
+        }
+        return new Action.PlaceTile(c, new BambooTile());
+    }
+
+    @BeforeEach
+    void setUp() throws BambooSizeException {
+        b1 = new BambooSizeObjective(1);
+        b3 = new BambooSizeObjective(3);
+        b4 = new BambooSizeObjective(4);
+        board = new Board();
+    }
+
+    @Test
+    void testBambooSizeObjectiveException() {
+        assertThrows(
+                BambooSizeException.class,
+                () -> {
+                    BambooSizeObjective b5 = new BambooSizeObjective(5);
+                });
+        assertThrows(
+                BambooSizeException.class,
+                () -> {
+                    BambooSizeObjective b0 = new BambooSizeObjective(0);
+                });
+    }
+
+    @Test
+    void testIsAchieved() {
+        assertFalse(b1.isAchieved(board, INITIAL_ACTION));
+        assertFalse(b3.isAchieved(board, INITIAL_ACTION));
+        assertFalse(b4.isAchieved(board, INITIAL_ACTION));
+
+        var secondAction = placeBambooTile(board, new Coord(0, 1));
+
+        Tile bt1 = null;
+        try {
+            bt1 = board.getTile(new Coord(0, 1));
+        } catch (BoardException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (bt1 instanceof BambooTile) {
+
+            // First bamboo grow
+            try {
+                ((BambooTile) bt1).growBamboo();
+            } catch (BambooSizeException e) {
+                fail(e);
+            }
+
+            // Now b1 is achieved
+            assertTrue(b1.isAchieved(board, secondAction));
+            assertFalse(b3.isAchieved(board, secondAction));
+
+            // Second bamboo grow
+            try {
+                ((BambooTile) bt1).growBamboo();
+            } catch (BambooSizeException e) {
+                fail(e);
+            }
+
+            // b1 is no longer achieved
+            assertFalse(b1.isAchieved(board, secondAction));
+
+            // Third bamboo grow
+            try {
+                ((BambooTile) bt1).growBamboo();
+            } catch (BambooSizeException e) {
+                fail(e);
+            }
+
+            // b3 is achieved
+            assertTrue(b3.isAchieved(board, secondAction));
+
+            // Fourth bamboo grow
+            try {
+                ((BambooTile) bt1).growBamboo();
+            } catch (BambooSizeException e) {
+                fail(e);
+            }
+
+            // b3 is no longer achieved, but b4 is
+            assertFalse(b3.isAchieved(board, secondAction));
+            assertTrue(b4.isAchieved(board, secondAction));
+        }
+    }
+}
