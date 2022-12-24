990d26f0a0b0af7fd7da03e0ed7c6ed6df9b1b4a
Edgar Bizel <edgar.bizel@etu.univ-cotedazur.fr>
2022-12-24 08:06:24 +0100

    test: improve EasyBot coverage #54

--- src/test/java/takenoko/bot/EasyBotTest.java
+++ src/test/java/takenoko/bot/EasyBotTest.java
@@ -2,11 +2,14 @@ package takenoko.bot;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.Random;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import takenoko.*;
+import takenoko.objective.Objective;
 
 class EasyBotTest {
     Random randomSource;
@@ -29,5 +32,21 @@ class EasyBotTest {
 
         var expectedAction = new Action.PlaceTile(new Coord(-1, 0), new BambooTile());
         assertEquals(expectedAction, action);
+    }
+
+    @Test
+    void unveilsObjectiveASAP() throws PlayerException {
+        Board board = new Board();
+        EasyBot bot = new EasyBot(randomSource);
+
+        var objMock = mock(Objective.class);
+        when(objMock.wasAchievedAfterLastCheck()).thenReturn(true);
+
+        bot.addObjective(objMock);
+
+        bot.beginTurn(1);
+        Action action = bot.chooseAction(board);
+
+        assertEquals(new Action.UnveilObjective(objMock), action);
     }
 }
