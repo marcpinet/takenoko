72d4c8257758a6288548390b1a64b79705676de8
Marc Pinet <marc.pinet@etu.univ-cotedazur.fr>
2022-12-30 16:58:16 +0100

    test: fixed a test that was forgotten during merge #23

--- src/test/java/takenoko/game/board/BoardTest.java
+++ src/test/java/takenoko/game/board/BoardTest.java
@@ -55,7 +55,7 @@ public class BoardTest {
         Coord c2 = new Coord(0, 2);
         Tile t1 = new BambooTile(Color.GREEN);
         tileboard.placeTile(c1, t1);
-        tileboard.placeTile(new Coord(1, 0), new BambooTile());
+        tileboard.placeTile(new Coord(1, 0), new BambooTile(Color.GREEN));
         // Gardener
         tileboard.move(MovablePiece.GARDENER, c1);
         assertEquals(tileboard.getGardenerCoord(), c1);
@@ -72,7 +72,7 @@ public class BoardTest {
         assertEquals(tileboard.getPandaCoord(), c1);
         assertThrows(BoardException.class, () -> tileboard.move(MovablePiece.PANDA, c2));
         Coord c4 = new Coord(1, 2);
-        Tile t3 = new BambooTile();
+        Tile t3 = new BambooTile(Color.GREEN);
         tileboard.placeTile(c2, new BambooTile(Color.GREEN));
         tileboard.placeTile(c4, t3);
         assertThrows(BambooSizeException.class, () -> tileboard.move(MovablePiece.PANDA, c4));
