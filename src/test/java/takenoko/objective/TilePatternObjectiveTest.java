fadc8801249b33a5dd0ff46ec5d5a2100dcfc67f
Edgar B <edgar.bizel@etu.univ-cotedazur.fr>
2023-01-04 22:09:27 +0100

    fix: TilePatternObjective and tile irrigation #69

--- src/test/java/takenoko/objective/TilePatternObjectiveTest.java
+++ src/test/java/takenoko/objective/TilePatternObjectiveTest.java
@@ -6,10 +6,9 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import takenoko.action.Action;
 import takenoko.game.board.Board;
+import takenoko.game.objective.Objective;
 import takenoko.game.objective.TilePatternObjective;
-import takenoko.game.tile.BambooTile;
-import takenoko.game.tile.Color;
-import takenoko.game.tile.TileDeck;
+import takenoko.game.tile.*;
 import takenoko.utils.Coord;
 
 class TilePatternObjectiveTest {
@@ -20,89 +19,87 @@ class TilePatternObjectiveTest {
     Action.PlaceTile placeBambooTile(Board board, Coord c) {
         try {
             board.placeTile(c, new BambooTile(Color.GREEN));
+            // irrigate all sides if possible
+            for (var side : TileSide.values()) {
+                try {
+                    board.placeIrrigation(c, side);
+                } catch (IrrigationException e) {
+                    // ignore
+                }
+            }
         } catch (Exception e) {
             fail(e);
         }
         return new Action.PlaceTile(c, TileDeck.DEFAULT_DRAW_TILE_PREDICATE);
     }
 
+    Board prepareBoard(Objective obj, Coord... coords) {
+        var board = new Board();
+        assertFalse(obj.isAchieved(board, INITIAL_ACTION));
+
+        for (var c : coords) {
+            var action = placeBambooTile(board, c);
+            assertFalse(obj.isAchieved(board, action));
+        }
+        return board;
+    }
+
     @Test
     void testLineOfTwo() {
         var objective = new TilePatternObjective(Color.GREEN, TilePatternObjective.LINE_2);
 
-        var board = new Board();
-        assertFalse(objective.isAchieved(board, INITIAL_ACTION));
+        var board = prepareBoard(objective, new Coord(-1, 0));
 
-        // place the second tile
-        var secondAction = placeBambooTile(board, new Coord(0, 1));
-
-        // Now objective is achieved
-        assertTrue(objective.isAchieved(board, secondAction));
+        var lastAction = placeBambooTile(board, new Coord(0, -1));
+        assertTrue(objective.isAchieved(board, lastAction));
     }
 
     @Test
     void testLineOfThree() {
         var objective = new TilePatternObjective(Color.GREEN, TilePatternObjective.LINE_3);
 
-        var board = new Board();
-        assertFalse(objective.isAchieved(board, INITIAL_ACTION));
+        // to achieve a line of 3, we need to place more than 3 tiles due to requirements
+        var board = prepareBoard(objective, new Coord(1, 0), new Coord(0, 1), new Coord(-1, 1));
 
-        // place the second tile
-        var secondAction = placeBambooTile(board, new Coord(0, 1));
-        assertFalse(objective.isAchieved(board, secondAction));
-
-        // Placing tiles, so we can place them without raising error adjacent
-        placeBambooTile(board, new Coord(1, 0));
-        placeBambooTile(board, new Coord(1, 1));
-
-        // place the third tile
-        var thirdAction = placeBambooTile(board, new Coord(0, 2));
-
-        // Now objective is achieved
-        assertTrue(objective.isAchieved(board, thirdAction));
+        var lastAction = placeBambooTile(board, new Coord(-1, 2));
+        assertTrue(objective.isAchieved(board, lastAction));
     }
 
     @Test
     void testSquareOfTwo() {
         var objective = new TilePatternObjective(Color.GREEN, TilePatternObjective.DIAMOND_4);
 
-        var board = new Board();
-        assertFalse(objective.isAchieved(board, INITIAL_ACTION));
+        var board =
+                prepareBoard(
+                        objective,
+                        new Coord(-1, 0),
+                        new Coord(0, -1),
+                        new Coord(-1, 1),
+                        new Coord(-2, 1));
 
-        // Place the other tiles
-        var secondAction = placeBambooTile(board, new Coord(0, 1));
-        assertFalse(objective.isAchieved(board, secondAction));
-        // On purpose rotated
-        var thirdAction = placeBambooTile(board, new Coord(1, 0));
-
-        assertFalse(objective.isAchieved(board, thirdAction));
-        var fourthAction = placeBambooTile(board, new Coord(-1, +1));
-        assertTrue(objective.isAchieved(board, fourthAction));
+        var lastAction = placeBambooTile(board, new Coord(-2, 0));
+        assertTrue(objective.isAchieved(board, lastAction));
     }
 
     @Test
     void testTriangleOfTwo() {
         var objective = new TilePatternObjective(Color.GREEN, TilePatternObjective.TRIANGLE_3);
 
-        var board = new Board();
-        assertFalse(objective.isAchieved(board, INITIAL_ACTION));
+        var board = prepareBoard(objective, new Coord(0, -1), new Coord(1, -1));
 
-        // Place the other tiles
-        var secondAction = placeBambooTile(board, new Coord(0, 1));
-        assertFalse(objective.isAchieved(board, secondAction));
-        var thirdAction = placeBambooTile(board, new Coord(1, 0));
-
-        // Now objective is achieved
-        assertTrue(objective.isAchieved(board, thirdAction));
+        var lastAction = placeBambooTile(board, new Coord(1, -2));
+        assertTrue(objective.isAchieved(board, lastAction));
     }
 
     @Test
     void patternScanOnlyTriggeredByPlaceTileAction() {
-        // always true
+        // true as soon as a tile is placed
         var objective = new TilePatternObjective(Color.GREEN, List.of(new Coord(0, 0)));
         var board = new Board();
+        var action = placeBambooTile(board, new Coord(0, 1));
+
         assertFalse(objective.isAchieved(board, Action.NONE));
-        assertTrue(objective.isAchieved(board, INITIAL_ACTION));
+        assertTrue(objective.isAchieved(board, action));
         // but once the objective is achieved, it stays achieved
         assertTrue(objective.isAchieved(board, Action.NONE));
     }
