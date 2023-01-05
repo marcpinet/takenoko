98ad8b0e69b813f1d4f592c9f66e693771e33694
Edgar B <edgar.bizel@etu.univ-cotedazur.fr>
2023-01-05 15:49:16 +0100

    test: add tests for TilePatternObjectiveTest #69

--- src/test/java/takenoko/objective/TilePatternObjectiveTest.java
+++ src/test/java/takenoko/objective/TilePatternObjectiveTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import takenoko.action.Action;
 import takenoko.game.board.Board;
+import takenoko.game.board.BoardException;
 import takenoko.game.objective.Objective;
 import takenoko.game.objective.TilePatternObjective;
 import takenoko.game.tile.*;
@@ -102,5 +103,60 @@ class TilePatternObjectiveTest {
         assertTrue(objective.isAchieved(board, action));
         // but once the objective is achieved, it stays achieved
         assertTrue(objective.isAchieved(board, Action.NONE));
+    }
+
+    @Test
+    void testDifferentColor() throws IrrigationException, BoardException {
+        var objective = new TilePatternObjective(Color.GREEN, TilePatternObjective.LINE_2);
+
+        var board = prepareBoard(objective, new Coord(-1, 0));
+
+        board.placeTile(new Coord(0, -1), new BambooTile(Color.PINK));
+        var action = new Action.PlaceTile(new Coord(0, -1), TileDeck.DEFAULT_DRAW_TILE_PREDICATE);
+
+        // the objective is not achieved because the color is different
+        assertFalse(objective.isAchieved(board, action));
+
+        // but if we place a green tile, it is achieved
+        board.placeTile(new Coord(-1, 1), new BambooTile(Color.GREEN));
+        var lastAction =
+                new Action.PlaceTile(new Coord(-1, 1), TileDeck.DEFAULT_DRAW_TILE_PREDICATE);
+        assertTrue(objective.isAchieved(board, lastAction));
+    }
+
+    @Test
+    void testNonIrrigated() throws IrrigationException, BoardException {
+        var objective = new TilePatternObjective(Color.PINK, TilePatternObjective.LINE_2);
+
+        // prepare board
+        var board = new Board();
+        board.placeTile(new Coord(-1, 0), new BambooTile(Color.GREEN));
+        board.placeTile(new Coord(0, -1), new BambooTile(Color.GREEN));
+
+        // placing before-last tile, irrigated
+        board.placeTile(new Coord(-1, -1), new BambooTile(Color.PINK));
+
+        var beforeLastTile = (BambooTile) board.getTile(new Coord(-1, -1));
+        beforeLastTile.irrigateSide(TileSide.UP_RIGHT);
+        assertTrue(beforeLastTile.isIrrigated());
+
+        // pattern not yet achieved
+        var action = new Action.PlaceTile(new Coord(-1, -1), TileDeck.DEFAULT_DRAW_TILE_PREDICATE);
+        assertFalse(objective.isAchieved(board, action));
+
+        // place the last tile, not irrigated
+        board.placeTile(new Coord(-2, 0), new BambooTile(Color.PINK));
+        var lastAction =
+                new Action.PlaceTile(new Coord(-2, 0), TileDeck.DEFAULT_DRAW_TILE_PREDICATE);
+        var lastTile = (BambooTile) board.getTile(new Coord(-2, 0));
+        assertFalse(lastTile.isIrrigated());
+
+        assertFalse(objective.isAchieved(board, action));
+
+        // but if we irrigate it, it is achieved
+        lastTile.irrigateSide(TileSide.UP_LEFT);
+        assertTrue(lastTile.isIrrigated());
+
+        assertTrue(objective.isAchieved(board, lastAction));
     }
 }
