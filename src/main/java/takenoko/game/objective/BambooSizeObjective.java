ea9d92d6177d05f4cdee21e84598fa57d3f2f01d
ClementREMY2 <clement.remy@etu.univ-cotedazur.fr>
2023-01-13 18:33:40 +0100

    fix: implement colors on BambooSizeObjective #86

--- src/main/java/takenoko/game/objective/BambooSizeObjective.java
+++ src/main/java/takenoko/game/objective/BambooSizeObjective.java
@@ -1,38 +1,65 @@
 package takenoko.game.objective;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import takenoko.action.Action;
 import takenoko.game.board.Board;
+import takenoko.game.board.BoardException;
 import takenoko.game.tile.BambooSizeException;
 import takenoko.game.tile.BambooTile;
+import takenoko.game.tile.Color;
+import takenoko.game.tile.Tile;
 import takenoko.player.Inventory;
 
 public class BambooSizeObjective implements Objective {
 
+    private final int numberOfBamboos;
     private final int sizeObjective;
+    private final Color color;
     private boolean achieved = false;
 
-    public BambooSizeObjective(int b) throws BambooSizeException {
-        if (b < 1 || b > 4) {
+    public BambooSizeObjective(int nbOfBamboos, int size, Color c) throws BambooSizeException {
+
+        boolean isVerified = false;
+
+        if (nbOfBamboos < 1 || nbOfBamboos > 4) {
+            throw new BambooSizeException("Error : unreachable number of bamboos.");
+        }
+        if (size < 1 || size > 4) {
             throw new BambooSizeException("Error : unreachable bamboo size.");
         }
-        this.sizeObjective = b;
+        Color[] colors = Color.values();
+        for (Color co : colors) {
+            if (c.equals(co)) {
+                isVerified = true;
+                break;
+            }
+        }
+        if (!isVerified) {
+            throw new BambooSizeException("Error : unknown color.");
+        }
+        this.numberOfBamboos = nbOfBamboos;
+        this.sizeObjective = size;
+        this.color = c;
     }
 
     @Override
     public boolean isAchieved(Board board, Action lastAction, Inventory ignored) {
-        AtomicBoolean res = new AtomicBoolean(false);
-        board.applyOnEachTile(
-                tile -> {
-                    if (!(tile instanceof BambooTile bambooTile)) {
-                        return null;
-                    }
-                    if (bambooTile.getBambooSize() == sizeObjective) {
-                        res.set(true);
-                    }
-                    return null;
-                });
-        achieved = res.get();
+        int nbOfBamboos = numberOfBamboos;
+        achieved = false;
+        for (var coord : board.getPlacedCoords()) {
+            Tile tile = null;
+            try {
+                tile = board.getTile(coord);
+            } catch (BoardException e) {
+                // TODO
+            }
+            if (tile instanceof BambooTile bambooTile
+                    && bambooTile.getBambooSize() == sizeObjective) {
+                nbOfBamboos--;
+            }
+        }
+        if (nbOfBamboos == 0) {
+            achieved = true;
+        }
         return achieved;
     }
 
