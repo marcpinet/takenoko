84fffe0de3b278d8b89abda66526f02cfe683df0
Edgar B <edgar.bizel@etu.univ-cotedazur.fr>
2023-01-25 15:56:16 +0100

    fix: make TilePatternObjective work in all cases

--- src/main/java/takenoko/game/objective/TilePatternObjective.java
+++ src/main/java/takenoko/game/objective/TilePatternObjective.java
@@ -13,12 +13,161 @@ import takenoko.game.tile.BambooTile;
 import takenoko.game.tile.Color;
 import takenoko.utils.Coord;
 
-/// This class works by storing a list of deltas from one edge of the pattern.
-/// For example, if the pattern is a 2x2 square, the deltas are:
-/// (0, 0), (0, 1), (1, 0), (1, 1)
-/// The pattern is then rotated to find all possible patterns.
-/// This way, we can check if a pattern is achieved by only checking starting from the last tile
-// placed.
+// spotless:off (spotless destroys the ascii art)
+/** This class works by storing a list of deltas from one edge of the pattern.
+For example, if the pattern is a 2x2 square, the deltas are:
+(0, 0), (0, 1), (1, 0), (1, 1)
+The pattern is then rotated to find all possible patterns.
+This way, we can check if a pattern is achieved by only checking starting from the last tile
+placed.
+# Algorithm
+The important part is the generation of the pattern variations.
+Let's use an example. Black hexagons are the tiles we want to match, white hexagons are the others
+The `DIAMOND_WITHOUT_RIGHT_PART`, which is :
+ ```raw
+  _____         _____         _____
+ /     \       /     \       /     \
+/ -2,0  \_____/  ===  \_____/  2,0  \
+\       /     \       /     \       /
+ \_____/  ===  \_____/  1,1  \_____/
+ /     \       /     \       /     \
+/ -2,1  \_____/  0,1  \_____/  2,1  \
+\       /     \       /     \       /
+ \_____/  ===  \_____/  1,2  \_____/
+ /     \       /     \       /     \
+/ -2,2  \_____/  0,2  \_____/  2,2  \
+\       /     \       /     \       /
+ \_____/       \_____/       \_____/
+```
+The deltas are:
+```
+(0, 0), (-1, 1), (-1, 2)
+```
+ The first step is to find all possible rotations of the pattern
+ Rotation 1:
+```raw
+  _____         _____         _____
+ /     \       /     \       /     \
+/ -2,0  \_____/  ===  \_____/  2,0  \
+\       /     \       /     \       /
+ \_____/ -1,1  \_____/  1,1  \_____/
+ /     \       /     \       /     \
+/ -2,1  \_____/  ===  \_____/  2,1  \
+\       /     \       /     \       /
+ \_____/ -1,2  \_____/  ===  \_____/
+ /     \       /     \       /     \
+/ -2,2  \_____/  0,2  \_____/  2,2  \
+\       /     \       /     \       /
+ \_____/       \_____/       \_____/
+``` Rotation 2:
+```raw
+  _____         _____         _____
+ /     \       /     \       /     \
+/ -2,0  \_____/  ===  \_____/  ===  \
+\       /     \       /     \       /
+ \_____/ -1,1  \_____/  ===  \_____/
+ /     \       /     \       /     \
+/ -2,1  \_____/  0,1  \_____/  2,1  \
+\       /     \       /     \       /
+ \_____/ -1,2  \_____/  1,2  \_____/
+ /     \       /     \       /     \
+/ -2,2  \_____/  0,2  \_____/  2,2  \
+\       /     \       /     \       /
+ \_____/       \_____/       \_____/
+``` Rotation 3:
+```raw
+         _____         _____
+        /     \       /     \
+  _____/ -1,-2 \_____/  1,-2 \_____
+ /     \       /     \       /     \
+/ -2,-2 \_____/  0,-2 \_____/  2,-2 \
+\       /     \       /     \       /
+ \_____/ -1,-1 \_____/  ===  \_____/
+ /     \       /     \       /     \
+/ -2,-1 \_____/  0,-1 \_____/  2,-1 \
+\       /     \       /     \       /
+ \_____/ -1,0  \_____/  ===  \_____/
+ /     \       /     \       /     \
+/ -2,0  \_____/  ===  \_____/  2,0  \
+\       /     \       /     \       /
+ \_____/ -1,1  \_____/  1,1  \_____/
+ /     \       /     \       /     \
+/ -2,1  \_____/  0,1  \_____/  2,1  \
+\       /     \       /     \       /
+ \_____/       \_____/       \_____/
+``` Rotation 4:
+```raw
+         _____         _____
+        /     \       /     \
+  _____/ -1,-2 \_____/  1,-2 \_____
+ /     \       /     \       /     \
+/ -2,-2 \_____/  0,-2 \_____/  2,-2 \
+\       /     \       /     \       /
+ \_____/  ===  \_____/  1,-1 \_____/
+ /     \       /     \       /     \
+/ -2,-1 \_____/  ===  \_____/  2,-1 \
+\       /     \       /     \       /
+ \_____/ -1,0  \_____/  1,0  \_____/
+ /     \       /     \       /     \
+/ -2,0  \_____/  ===  \_____/  2,0  \
+\       /     \       /     \       /
+ \_____/ -1,1  \_____/  1,1  \_____/
+ /     \       /     \       /     \
+/ -2,1  \_____/  0,1  \_____/  2,1  \
+\       /     \       /     \       /
+ \_____/       \_____/       \_____/
+``` Rotation 5:
+```raw
+         _____         _____
+        /     \       /     \
+  _____/ -1,-2 \_____/  1,-2 \_____
+ /     \       /     \       /     \
+/ -2,-2 \_____/  0,-2 \_____/  2,-2 \
+\       /     \       /     \       /
+ \_____/ -1,-1 \_____/  1,-1 \_____/
+ /     \       /     \       /     \
+/ -2,-1 \_____/  0,-1 \_____/  2,-1 \
+\       /     \       /     \       /
+ \_____/  ===  \_____/  1,0  \_____/
+ /     \       /     \       /     \
+/  ===  \_____/  ===  \_____/  2,0  \
+\       /     \       /     \       /
+ \_____/ -1,1  \_____/  1,1  \_____/
+ /     \       /     \       /     \
+/ -2,1  \_____/  0,1  \_____/  2,1  \
+\       /     \       /     \       /
+ \_____/       \_____/       \_____/
+ ```
+There we go for the rotations... or do we?
+This is not enough! We need to do the rotations, starting from the second tile too!
+ For example, Rotation 1.1
+```raw
+  _____         _____         _____
+ /     \       /     \       /     \
+/ -2,0  \_____/  ===  \_____/  2,0  \
+\       /     \       /     \       /
+ \_____/ -1,1  \_____/  1,1  \_____/
+ /     \       /     \       /     \
+/ -2,1  \_____/  ===  \_____/  2,1  \
+\       /     \       /     \       /
+ \_____/  ===  \_____/  ---  \_____/
+ /     \       /     \       /     \
+/ -2,2  \_____/  0,2  \_____/  2,2  \
+\       /     \       /     \       /
+ \_____/       \_____/       \_____/
+ ```
+The "---" represents the original tile, and the "===" represents the new one.
+As we can see, the second tile has moved independently from the first one
+Fortunately, we do not have to generate more rotations. If we look more closely, we can see that Rotation 1.1 is
+the same as Rotation 3, with a translation. Translations are handled easily thanks to our use of offsets.
+
+ Generation of the rotations is done by `generateRotations`
+
+However, we're not done yet. The algorithm has to be able to start from any tile, and not only from the first one.
+So this means we have to use each tile in the pattern as the starting point for the algorithm.
+This is handled by `generateShifts`
+**/
+// spotless:on
 public class TilePatternObjective implements Objective {
     private record Element(Color color, Coord coord) {
         public Element offset(Coord offset) {
@@ -27,10 +176,6 @@ public class TilePatternObjective implements Objective {
 
         public Element rotate(Coord center) {
             return new Element(color, coord.rotate(center));
-        }
-
-        public Element reversed() {
-            return new Element(color, coord.reversed());
         }
     }
 
@@ -61,13 +206,11 @@ public class TilePatternObjective implements Objective {
 
         // initial pattern without rotation
         patternVariations =
-                generateShifts(patternWithColors).stream()
+                generateRotations(patternWithColors).stream()
                         .unordered()
                         // add the shifted patterns
-                        .flatMap(pat -> generateRotations(pat).stream())
-                        // add the reversed patterns
-                        .flatMap(pat -> generateReversed(pat).stream())
-                        // remove duplicates
+                        .flatMap(pat -> generateShifts(pat).stream())
+                        // remove potential duplicates
                         .collect(Collectors.toSet());
         this.score = score;
     }
@@ -83,46 +226,33 @@ public class TilePatternObjective implements Objective {
 
     /// Generate all possible rotations of a pattern
     private List<List<Element>> generateRotations(List<Element> pattern) {
-        var rotations = new ArrayList<List<Element>>();
-        rotations.add(pattern);
+        var all_rotations = new ArrayList<List<Element>>();
+        all_rotations.add(pattern);
 
         // rotate the pattern to find all possible patterns
         final int ROTATION_COUNT = 6;
         for (int i = 1; i < ROTATION_COUNT; ++i) {
-            var rotated = new ArrayList<Element>();
-            rotated.add(pattern.get(0));
+            var current_rotation = new ArrayList<Element>();
+            current_rotation.add(pattern.get(0));
             for (int j = 1; j < pattern.size(); ++j) {
-                rotated.add(rotations.get(i - 1).get(j).rotate(rotated.get(0).coord()));
+                current_rotation.add(
+                        all_rotations.get(i - 1).get(j).rotate(current_rotation.get(0).coord()));
             }
-            rotations.add(rotated);
+            all_rotations.add(current_rotation);
         }
-        return rotations;
+        return all_rotations;
     }
 
-    /// Generate all possible patterns by shifting the pattern by one tile.
+    /// Generate shifted patterns. That is to say, patterns with the same elements, but with
+    // different origins
     private List<List<Element>> generateShifts(List<Element> pattern) {
-        var shifts = new ArrayList<List<Element>>();
-        shifts.add(pattern);
-        for (int i = 1; i < pattern.size(); ++i) {
-            var shifted = new ArrayList<Element>();
-            for (int j = 0; j < pattern.size(); ++j) {
-                shifted.add(shifts.get(i - 1).get((j + 1) % pattern.size()));
-            }
-            shifts.add(shifted);
+        var all_shifts = new ArrayList<List<Element>>();
+        for (int i = 0; i < pattern.size(); ++i) {
+            var offset = pattern.get(i).coord().negated();
+            var current_shift = pattern.stream().map(e -> e.offset(offset)).toList();
+            all_shifts.add(current_shift);
         }
-        return shifts;
-    }
-
-    /// Generate all possible patterns by reversing the pattern.
-    private List<List<Element>> generateReversed(List<Element> pattern) {
-        var reversed = new ArrayList<List<Element>>();
-        var revertedPattern = new ArrayList<Element>();
-        for (var el : pattern) {
-            revertedPattern.add(el.reversed());
-        }
-        reversed.add(revertedPattern);
-        reversed.add(pattern);
-        return reversed;
+        return all_shifts;
     }
 
     @Override
