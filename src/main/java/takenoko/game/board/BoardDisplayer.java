f3d870149b5047407d35470891f9234468fb6adc
Edgar B <edgar.bizel@etu.univ-cotedazur.fr>
2023-02-07 19:34:43 +0100

    refactor: extract BoardDisplay of Board #176
    It is excluded from coverage as only used for debugging purposes

---
+++ src/main/java/takenoko/game/board/BoardDisplayer.java
@@ -0,0 +1,55 @@
+package takenoko.game.board;
+
+import java.util.Optional;
+import takenoko.game.tile.BambooTile;
+import takenoko.game.tile.Tile;
+
+public class BoardDisplayer {
+    private final Board board;
+
+    public BoardDisplayer(Board board) {
+        this.board = board;
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = -2; i < 3; i++) {
+            for (int j = -2; j < 3; j++) {
+                var tile = getTileAtDisplayPosition(i, j);
+                if (tile.isPresent()) {
+                    if (tile.get() instanceof BambooTile bambooTile) {
+                        sb.append(' ')
+                                .append(bambooTile.getColor().toString().charAt(0))
+                                .append(' ');
+                    } else {
+                        sb.append("---");
+                    }
+                    sb.append("(").append(i).append(",").append(j).append(") ");
+
+                } else {
+                    sb.append("-------- ");
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    private Optional<Tile> getTileAtDisplayPosition(int i, int j) {
+        for (var c : board.getPlacedCoords()) {
+
+            var row = c.x();
+            var col = c.y() + (c.x() + (c.x() & 1)) / 2;
+            if (row == i && col == j) {
+                try {
+                    return Optional.of(board.getTile(c));
+                } catch (BoardException e) {
+                    throw new IllegalStateException();
+                }
+            }
+        }
+        return Optional.empty();
+    }
+}
